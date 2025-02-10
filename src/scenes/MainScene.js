16f679f577cb327c52b16e56c5f7922c49a935b2
Neffer2 <n3f73r@gmail.com>
2025-02-10 17:42:26 -0500

    1. Avances tierra infinita

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -8,6 +8,8 @@ export class MainScene extends Phaser.Scene {
     initGrassPosition = 200;
     space_between_grass = [290, 435];
     initCameraScrollX; 
+    singleJumpBtn;
+    doubleJumpBtn;
 
     constructor(){
         super('MainScene');
@@ -18,22 +20,27 @@ export class MainScene extends Phaser.Scene {
     create(){
         this.mContext = this;
         this.initCameraScrollX = this.camera.scrollX;
+
+        this.singleJumpBtn.on('pointerdown', () => {
+            this.player.x += 290;
+        });
+
+        this.doubleJumpBtn.on('pointerdown', () => {
+            this.player.x += 435;
+        });
     }
     
     update(){
-        this.player.setVelocityX(0);
-
         this.grass.forEach((grass) => {
-            if (this.player.x - grass.x  === 300){
+            // if (grass.key == 0){
+            //     console.log("grass x: "+grass.x);
+            //     console.log("Player x: "+this.player.x);
+            // }
+            if (this.player.x - grass.x  >= 290){
+                console.log("grass reset"); 
                 this.resetGrass(grass);
             }
         });
-    
-        if (this.cursors.right.isDown){
-            this.player.setVelocityX(300);
-        }if (this.cursors.left.isDown){
-            this.player.setVelocityX(-300);
-        }
     }
     
     init(){
@@ -42,6 +49,8 @@ export class MainScene extends Phaser.Scene {
         this.add.image(0, 0, 'background').setScale(.8).setOrigin(0).setScrollFactor(0);
         this.cursors = this.input.keyboard.createCursorKeys();
         this.player = this.physics.add.sprite(200, ((this.height/2) + 150), 'player');
+        this.singleJumpBtn = this.add.image((this.width/2) - 150, (this.height - 150), 'play-btn').setScale(.5).setInteractive().setScrollFactor(0);
+        this.doubleJumpBtn = this.add.image((this.width/2) + 150, (this.height - 150), 'play-btn').setScale(.5).setInteractive().setScrollFactor(0);
         this.setGrass();
         this.camera = this.cameras.main;
         this.camera.startFollow(this.player, true, .5, 1, -(this.player.x), 100);
@@ -68,19 +77,37 @@ export class MainScene extends Phaser.Scene {
         for (let i = 0; i < 3; i++){
             if (i == 0){
                 temp_grass = this.physics.add.sprite(this.initGrassPosition, (this.height/2) + 300, 'grass-'+this.getRandomInt(1, 3));
+                temp_grass.key = i;
             }else {
                 temp_grass = this.physics.add.sprite(
                     (this.grass[this.grass.length-1].x) + this.space_between_grass[this.getRandomInt(0, 2)], 
                     (this.height/2) + 300, 
                     'grass-'+this.getRandomInt(1, 3));
+                temp_grass.key = i;
             }
             this.grass.push(temp_grass);
         }
     }
 
     resetGrass(grass){
-        grass.x = (this.grass[this.grass.length-1].x) + this.space_between_grass[this.getRandomInt(0, 2)];
+        this.grass[this.grass.length-1].setTint(0xff0000);
+        setTimeout(() => {
+            this.grass[this.grass.length-1].setTint(0x000000);
+        }, 1000);  
+        grass.x = (this.grass[this.grass.length-1]).x + this.space_between_grass[this.getRandomInt(0, 2)];
         grass.setTexture('grass-'+this.getRandomInt(1, 3));
+
+        this.swap(this.grass, grass.key, this.grass[this.grass.length-1].key);
+
+        this.grass.forEach((grass) => {
+            console.log(grass.key);
+        });
+    }
+
+    swap(arr, index1, index2) {
+        let temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
     gameOver(){
